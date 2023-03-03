2ba57cf37f6d5f2ac35371d19f698cb9a7147205
Edward <75829088+edwardgazitonline@users.noreply.github.com>
2023-03-03 15:08:42 +0200

    first commit

---
+++ src/app/greet/greet.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-greet',
+  templateUrl: './greet.component.html',
+  styleUrls: ['./greet.component.css']
+})
+export class GreetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  name: string = "Steve";
+
+  greet(): void {
+      alert("Hello " + this.name);
+  };
+}
